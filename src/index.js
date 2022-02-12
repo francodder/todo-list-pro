a29e491d18a36416932ca38a36e03ba1d89235f7
franRicoso <frocchia@encodesa.com.ar>
2022-02-12 15:34:18 -0300

    Migración de SASS a SCSS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoListApp from './TodoListApp';
-import './styles/main.sass'
+import './styles/main.scss'
 
 ReactDOM.render(
   <React.StrictMode>
